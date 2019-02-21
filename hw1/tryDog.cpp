69537f8a60b4c8e51a95fa9a31cb4b15bf177dc7
Eric Johnson <johnso94@stolaf.edu>
2019-02-21 13:17:24 -0600

    this is homework 1

---
+++ hw1/tryDog.cpp
@@ -0,0 +1,15 @@
+/*Exaple progrm for CS 251 R. Brown.rev 2/15 */
+#include<iostream>
+using namespace std;
+#include "Dog.h"
+
+/* main program */
+int main() {
+    Dog mydog("Fido", 3);
+
+    cout << "Hello, world!" <<endl;
+    cout << "My dog is named " <<mydog.get_name()
+    << ", and it is " << mydog.get_age() << " years old." << endl;
+
+    return 0; /*indicates successful run of the program*/
+}
