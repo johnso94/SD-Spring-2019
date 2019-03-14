00ca45acedaa6385d32c85fba62758679f5a3260
Eric Johnson <johnso94@stolaf.edu>
2019-03-14 14:21:48 -0500

    lab4A: complete

---
+++ images/same2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+struct Pixel{
+    unsigned char red;
+    unsigned char blue;
+    unsigned char green;
+    
+};
+void write_ppm(char * format_name, int x ,int y, int range, unsigned char * data){
+    cout << format_name << endl;
+    cout << x << " " << y << endl << range << endl;
+    cout.write((char *)data, sizeof(Pixel)*x*y);
+}
+int main(){
+    char fname[5];
+    int x, y, range;
+    cin >> fname;
+    cin >> x >> y >> range;
+    Pixel** Images = new Pixel*[y];
+    for(int i=0; i<y; i++){
+        Images[i] = new Pixel[x];
+    }
+    for(int i=0; i < y; i++){
+        cin.read( (char *) Images[i], x*sizeof(Pixel));
+    }
+    cout
+    for(int i=0; i<y; i++){
+        cout.write((char *)Images, sizeof(Pixel)*x);
+    }
+}
